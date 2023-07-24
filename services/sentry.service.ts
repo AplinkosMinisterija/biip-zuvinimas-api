f46c58ae5e1ace9020d3287c5ee8b4155f4aa6b9
Karolis Vyčius <k.vycius@gmail.com>
2023-07-24 12:02:53 +0300

    Sentry performance monitoring

--- services/sentry.service.ts
+++ services/sentry.service.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 import SentryMixin from 'moleculer-sentry';
+import Sentry from '@sentry/node';
 
 module.exports = {
   mixins: [SentryMixin],
@@ -9,11 +10,18 @@ module.exports = {
     sentry: {
       /** @type {String} DSN given by sentry. */
       dsn: process.env.SENTRY_DSN,
+
       /** @type {String} Name of event fired by "Event" exported in tracing. */
       tracingEventName: '$tracing.spans',
       /** @type {Object} Additional options for `Sentry.init`. */
       options: {
         environment: process.env.NODE_ENV,
+        tracesSampleRate: 1,
+        integrations: [
+          // enable HTTP calls tracing
+          new Sentry.Integrations.Http({ tracing: true }),
+          new Sentry.Integrations.Postgres(),
+        ],
       },
       /** @type {String?} Name of the meta containing user infos. */
       userMetaKey: 'authUser',
